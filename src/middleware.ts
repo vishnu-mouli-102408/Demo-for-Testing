1b4852581d86993e6b3960b6a360a5bbab7c95bd
Vishnu Mouli <vishnumouli0@gmail.com>
2024-01-31 21:19:34 +0530

    updated middleware file

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse, NextRequest } from "next/server";
+
+// The country to block from accessing the secret page
+const BLOCKED_COUNTRY = "SE";
+
+// Trigger this middleware to run on the `/secret-page` route
+export const config = {
+  matcher: "/secret",
+};
+
+export default function middleware(request: NextRequest) {
+  // Extract country. Default to US if not found.
+  const country = (request.geo && request.geo.country) || "US";
+
+  console.log(`Visitor from ${country}`);
+
+  // Specify the correct route based on the requests location
+  if (country === BLOCKED_COUNTRY) {
+    return NextResponse.redirect("/client");
+  } else {
+    return NextResponse.redirect("/secret");
+  }
+
+  // Rewrite to URL
+  return NextResponse.rewrite(request.url);
+}
