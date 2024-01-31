cd7e13e01b8fcba35a07117e4c401d0aec9dd0f8
Vishnu Mouli <vishnumouli0@gmail.com>
2024-01-31 21:40:52 +0530

    updated country code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 
 // The country to block from accessing the secret page
-const BLOCKED_COUNTRY = "SE";
+const BLOCKED_COUNTRY = "IN";
 
 // Trigger this middleware to run on the `/secret-page` route
 export const config = {
